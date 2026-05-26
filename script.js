dcb5cd6669b3ff6a67b5f28a5445e952abde72d0
Japón y su cultura <joaquina.devoto@comunidad.ub.edu.ar>
2026-05-26 14:33:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const toggle = document.getElementById('menu-toggle')
+const menu = document.getElementById('menu')
 
+toggle.addEventListener('click', function() {
+    menu.classList.toggle('abierto')
+})
